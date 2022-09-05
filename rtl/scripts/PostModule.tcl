9b4b781bfbbc7b22e00aa563694d3aa897fd756c
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2022-09-05 14:15:48 -0700

    quartus: start pi detached

--- rtl/scripts/PostModule.tcl
+++ rtl/scripts/PostModule.tcl
@@ -6,5 +6,5 @@ if [string match $module "quartus_asm"] {
     post_message "Programming device"
     qexec "quartus_pgm output_files/PET.cdf"
     post_message "Restarting target"
-    qexec "ssh pi@rpi3.local \"sudo killall -9 main; sudo ./main || true\""
+    qexec "ssh pi@rpi3.local \"sudo killall -9 main; nohup sudo ./main </dev/null >/dev/null 2>/dev/null &\""
 }
