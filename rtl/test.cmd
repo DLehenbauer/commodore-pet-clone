2346c3ab97eb2daa631118f31cbcb282fb709760
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2022-09-09 07:31:16 -0700

    test.cmd

--- rtl/test.cmd
+++ rtl/test.cmd
@@ -1,4 +1,4 @@
-setlocal /ENABLEDELAYEDEXPANSION
+setlocal ENABLEDELAYEDEXPANSION
 :: iverilog -g2012 crtc.v tb_crtc.v tb_common.v && vvp a.out || exit /b !ERRORLEVEL!
 iverilog -g2012 video.v bus.v tb_video.v tb_common.v && vvp a.out || exit /b !ERRORLEVEL!
 iverilog -g2012 bus.v tb_bus.v tb_common.v && vvp a.out || exit /b !ERRORLEVEL!
