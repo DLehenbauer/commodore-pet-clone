12317cc81407145080a2a3adc966ffce7d52814e
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2022-09-11 18:42:36 +0000

    fw: wip: spi

--- fw/main.c
+++ fw/main.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "pico/binary_info.h"
+#include "hardware/spi.h"
 
 int main() {
     stdio_init_all();
 
-    while (true) {
-        printf("UART via Picoprobe.\n");
-        sleep_ms(1000);
-    }
+    spi_init(spi0, /* 50 MHz */ 50 * 1000 * 1000);
+    spi_set_slave(spi0, /* slave: */ true);
     
+    gpio_set_function(2, GPIO_FUNC_SPI);
+    gpio_set_function(3, GPIO_FUNC_SPI);
+    gpio_set_function(4, GPIO_FUNC_SPI);
+    gpio_set_function(5, GPIO_FUNC_SPI);
+
+    uint8_t bytes [] = { 0xd4 };
+
+    while (true) {
+        spi_read_blocking(spi0, /* tx: */ 0, bytes, sizeof(bytes));
+        sleep_ms(100);
+    }
+
     return 0;
 }
