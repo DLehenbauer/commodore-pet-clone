068e31426076ab852b5d5fa6f38dcc45db7919e1
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2022-09-12 07:02:30 -0700

    tb_common: remove redundant ERROR string

--- rtl/tb_common.v
+++ rtl/tb_common.v
@@ -4,7 +4,7 @@ task assert_equal(
     input string name
 );
     if (actual !== expected) begin
-        $error("FAIL: '%s' must be %d, but got %d.", name, expected, actual);
+        $error("'%s' must be %d, but got %d.", name, expected, actual);
         $stop;
     end
 endtask
