2297b675eac8dccccb94f86861cc15ec64e1128d
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2022-09-17 14:16:52 -0700

    Use system verilog

--- rtl/test.cmd
+++ rtl/test.cmd
@@ -1,9 +1,9 @@
 setlocal ENABLEDELAYEDEXPANSION
-:: iverilog -g2012 crtc.v tb_crtc.v tb_common.v && vvp a.out || exit /b !ERRORLEVEL!
-iverilog -g2012 spi.v tb_spi_pi.v tb_common.v && vvp a.out || exit /b !ERRORLEVEL!
-iverilog -g2012 spi.v tb_spi.v tb_common.v && vvp a.out || exit /b !ERRORLEVEL!
-iverilog -g2012 video.v bus.v tb_video.v tb_common.v && vvp a.out || exit /b !ERRORLEVEL!
-iverilog -g2012 bus.v tb_bus.v tb_common.v && vvp a.out || exit /b !ERRORLEVEL!
-iverilog -g2012 keyboard.v tb_keyboard.v tb_common.v && vvp a.out || exit /b !ERRORLEVEL!
-iverilog -g2012 sync.v bus.v tb_sync.v tb_common.v && vvp a.out || exit /b !ERRORLEVEL!
-iverilog -g2012 address_decoding.v tb_address_decoding.v tb_common.v && vvp a.out || exit /b !ERRORLEVEL!
+:: iverilog -g2012 crtc.sv tb_crtc.sv tb_common.sv && vvp a.out || exit /b !ERRORLEVEL!
+iverilog -g2012 spi.sv tb_spi_pi.sv tb_common.sv && vvp a.out || exit /b !ERRORLEVEL!
+iverilog -g2012 spi.sv tb_spi.sv tb_common.sv && vvp a.out || exit /b !ERRORLEVEL!
+iverilog -g2012 video.sv bus.sv tb_video.sv tb_common.sv && vvp a.out || exit /b !ERRORLEVEL!
+iverilog -g2012 bus.sv tb_bus.sv tb_common.sv && vvp a.out || exit /b !ERRORLEVEL!
+iverilog -g2012 keyboard.sv tb_keyboard.sv tb_common.sv && vvp a.out || exit /b !ERRORLEVEL!
+iverilog -g2012 sync.sv bus.sv tb_sync.sv tb_common.sv && vvp a.out || exit /b !ERRORLEVEL!
+iverilog -g2012 address_decoding.sv tb_address_decoding.sv tb_common.sv && vvp a.out || exit /b !ERRORLEVEL!
