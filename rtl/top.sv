357ac9636c6757d0bf044a4c790938c2ea372c55
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2022-11-15 05:07:05 -0800

    top: more cleanup

--- rtl/top.sv
+++ rtl/top.sv
@@ -13,6 +13,9 @@
  */
 
 module top(
+    // FPGA
+    input logic          clk_50_i,        // 50 MHz oscillator
+
     // System Bus
     inout wire           bus_rw_nio,    // CPU 34          : 0 = CPU writing, 1 = CPU reading
     inout wire   [16:0]  bus_addr_io,   // CPU 9-20, 22-25 : System address bus
@@ -38,11 +41,11 @@ module top(
     output logic ram_we_no,             // RAM 29 : 0 = write enabled,  1 = Not active
 
     // CPU
-    inout  logic cpu_res_naio,          // CPU 40 : 0 = reset, 1 = normal operation
-    output logic cpu_ready_o,           // CPU  2 : 0 = halt,  1 = run
-    inout  wire  cpu_irq_nio,           // CPU  4 : 0 = interrupt requested, 1 = normal operation
-    inout  wire  cpu_nmi_nio,           // CPU  6 : 0 = interrupt requested, 1 = normal operation
-    input  wire  cpu_sync_i,
+    inout  logic cpu_res_naio,          // CPU 40 : 0 = Reset, 1 = Normal [Open drain]
+    output logic cpu_ready_o,           // CPU  2 : 0 = Halt,  1 = Run
+    inout  wire  cpu_irq_nio,           // CPU  4 : 0 = Interrupt requested, 1 = Normal [Open drain]
+    inout  wire  cpu_nmi_nio,           // CPU  6 : 0 = Interrupt requested, 1 = Normal [Open drain]
+    input  wire  cpu_sync_i,            // CPU  7 :
 
     // Address Decoding
     output logic cpu_en_no,            // CPU 36 : 1 = High impedance,  0 = enabled (be)
@@ -53,49 +56,50 @@ module top(
     output logic io_oe_no,
 
     // Audio
-    input diag_i,
-    input cb2_i,
-    output audio_o,
+    input  logic diag_i,
+    input  logic cb2_i,
+    output logic audio_o,
 
     // Graphics
-    input gfx_i,
-    output hsync_o,
-    output vsync_o,
-    output video_o,
+    input  logic gfx_i,
+    output logic hsync_o,
+    output logic vsync_o,
+    output logic video_o,
 
     // DEBUG
-    output [7:0] pi_data,
+    output logic [7:0] pi_data,
     
     // Reserved by DevBoard
     // (See http://land-boards.com/blwiki/index.php?title=Cyclone_II_EP2C5_Mini_Dev_Board#I.2FO_Pin_Mapping)
-    output P3_LED_D2,           // Low to Light LED
-    output P7_LED_D4,           // Low to Light LED
-    output P9_LED_D5,           // Low to Light LED
-    input  P17_50MHz,           // Clock input
-    input  P26_1V2,             // VCC 1.2V for EP2C8.  On EP2C5, remove "Zero ohm" resistor to use pin used as normal.
-    input  P27_GND,             // GND for EP2C8.  On EP2C5, remove "Zero ohm" resistor to use pin used as normal.
-    input  P73_POR              // 10uF capacitor to ground + 10K resistor to Vcc (Presumably for power up reset?)
+    output logic P3_LED_D2,           // Low to Light LED
+    output logic P7_LED_D4,           // Low to Light LED
+    output logic P9_LED_D5,           // Low to Light LED
+    input  logic P26_1V2,             // VCC 1.2V for EP2C8.  On EP2C5, remove "Zero ohm" resistor to use pin used as normal.
+    input  logic P27_GND,             // GND for EP2C8.  On EP2C5, remove "Zero ohm" resistor to use pin used as normal.
+    input  logic P73_POR              // 10uF capacitor to ground + 10K resistor to Vcc (Presumably for power up reset?)
     
     // The following reserved Pins are currently in use:
-    //    P80/81 - Removed R9/R10 and used from ram_addr
-    //    P144   - Used for cpu_res_naio
+    //    P17_50MHz - 50 MHz oscillator (clk_50_i)
+    //    P80/81    - Removed R9/R10 and used for ram_addr_o[11:10]
+    //    P144      - Used for cpu_res_naio
     //
-    // input  P80_GND,             // GND for EP2C8.  On EP2C5, remove "Zero ohm" resistor to use pin used as normal.
-    // input  P81_1V2              // VCC 1.2Vfor EP2C8.  On EP2C5, remove "Zero ohm" resistor to use pin used as normal.
-    // inout P144_KEY              // Push to ground.  Requires internal pullup on FPGA if used.
+    // input logic  P17_50MHz,           // 50 MHz oscillator
+    // input logic  P80_GND,             // GND for EP2C8.  On EP2C5, remove "Zero ohm" resistor to use pin used as normal.
+    // input logic  P81_1V2              // VCC 1.2Vfor EP2C8.  On EP2C5, remove "Zero ohm" resistor to use pin used as normal.
+    // inout logic P144_KEY              // Push to ground.  Requires internal pullup on FPGA if used.
 );
-    wire pi_pending = !spi_pending_ni;
-    wire pi_done;
+    logic pi_pending, pi_done;
+    assign pi_pending  = !spi_pending_ni;
     assign spi_done_no = !pi_done;
 
-    wire res_b;
-    wire irq_b = 1'b1;
-    wire nmi_b = 1'b1;
+    logic res_n = 1'b0;
+    logic irq_n = 1'b1;
+    logic nmi_n = 1'b1;
 
-    // res_b, irq_b, and nmi_b are open drain for wire-OR (see also *.qsf)
-    assign cpu_res_naio = res_b ? 1'bZ : 1'b0;
-    assign cpu_irq_nio = irq_b ? 1'bZ : 1'b0;
-    assign cpu_nmi_nio = nmi_b ? 1'bZ : 1'b0;
+    // Note: RESB, IRQB, and NMIB are open drain / wire-OR (see also *.qsf)
+    assign cpu_res_naio = res_n ? 1'bZ : 1'b0;
+    assign cpu_irq_nio  = irq_n ? 1'bZ : 1'b0;
+    assign cpu_nmi_nio  = nmi_n ? 1'bZ : 1'b0;
 
     assign P3_LED_D2 = spi_pending_ni;
     assign P7_LED_D4 = spi_done_no;
@@ -104,7 +108,7 @@ module top(
     wire clk16;     // 16 MHz clock from PLL
     
     pll pll(
-        .inclk0(P17_50MHz),
+        .inclk0(clk_50_i),
         .c0(clk16)
     );
     
@@ -123,12 +127,12 @@ module top(
     // assign pi_data[2] = spi_rx;
     // assign pi_data[3] = spi_tx;
 
-    wire pi_rw_b;
-    wire [16:0] pi_addr;
-    wire [7:0] pi_wr_data;          // Incoming data when Pi is writing
-    wire [7:0] pi_rd_data;          // Outgoing data when Pi is reading
-    wire pi_pending_out;
-    wire pi_done_in;
+    logic pi_rw_b;
+    logic [16:0] pi_addr;
+    logic [7:0] pi_wr_data;          // Incoming data when Pi is writing
+    logic [7:0] pi_rd_data;          // Outgoing data when Pi is reading
+    logic pi_pending_out;
+    logic pi_done_in;
 
     pi_com pi_com(
         .sys_clk(clk16),
@@ -166,7 +170,7 @@ module top(
         .pi_pending(pi_pending_out),
         .pi_done(pi_done_in),
         .reset_in(!cpu_res_naio),
-        .res_b_out(res_b),
+        .res_b_out(res_n),
         .cpu_rdy(cpu_ready_o),
         .cpu_sync(cpu_sync_i),
         .cpu_be(cpu_en_no),
