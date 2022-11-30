5172fcc5f9d0b30fbde79b02dff2dd18d75874ef
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2022-11-30 11:54:53 -0800

    rtl: update tb_crtc.sv

--- rtl/test.cmd
+++ rtl/test.cmd
@@ -1,7 +1,7 @@
 setlocal ENABLEDELAYEDEXPANSION
 iverilog -g2012 tb_dotgen.sv crtc.sv tb_common.sv && vvp a.out || exit /b !ERRORLEVEL!
 iverilog -g2012 tb_crtc_sync_gen.sv crtc.sv tb_common.sv && vvp a.out || exit /b !ERRORLEVEL!
-iverilog -g2012 tb_crtc.sv crtc.sv address_decoding.sv tb_common.sv && vvp a.out || exit /b !ERRORLEVEL!
+iverilog -g2012 tb_crtc.sv crtc.sv address_decoding.sv && vvp a.out || exit /b !ERRORLEVEL!
 iverilog -g2012 tb_spi.sv spi.sv pe_pulse.sv tb_common.sv && vvp a.out || exit /b !ERRORLEVEL!
 iverilog -g2012 tb_spi_bridge.sv spi_bridge.sv spi.sv spi_driver.sv && vvp a.out || exit /b !ERRORLEVEL!
 iverilog -g2012 tb_video.sv video.sv bus.sv tb_common.sv && vvp a.out || exit /b !ERRORLEVEL!
