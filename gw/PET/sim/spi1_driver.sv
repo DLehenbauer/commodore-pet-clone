2095667ae2d07485ea09801da769457a6b9671f8
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2023-09-26 07:32:58 -0700

    gw: move verification from spi1_driver to _tb

--- gw/PET/sim/spi1_driver.sv
+++ gw/PET/sim/spi1_driver.sv
@@ -16,45 +16,20 @@ module spi1_driver #(
     parameter CLK_MHZ = 64,         // Speed of destination clock
     parameter SCK_MHZ = 24          // SPI baud rate
 ) (
-    input  logic        clk_i,
-
-    output logic [16:0] addr_o,
-    input  logic  [7:0] rd_data_i,
-    output logic  [7:0] wr_data_o,
-    output logic        we_o,
-    output logic        cycle_o
+    output logic spi_sck_o,
+    output logic spi_cs_no,
+    output logic spi_pico_o,
+    input  logic spi_poci_i
 );
-    logic sck;
-    logic cs_n;
-    logic pico;
-    logic poci;
-
     spi_driver #(CLK_MHZ, SCK_MHZ) spi(
-        .spi_sck_o(sck),
-        .spi_cs_no(cs_n),
-        .spi_rx_i(poci),
-        .spi_tx_o(pico)
-    );
-
-    logic spi_ready;
-    logic ack = '0;
-
-    spi1_master spi1_master(
-        .spi_sck_i(sck),
-        .spi_cs_ni(cs_n),
-        .spi_rx_i(pico),
-        .spi_tx_o(poci),
-        .spi_ready_o(spi_ready),
-
-        .clk_i(clk_i),
-        .addr_o(addr_o),
-        .rd_data_i(rd_data_i),
-        .wr_data_o(wr_data_o),
-        .we_o(we_o),
-        .cycle_o(cycle_o)
+        .spi_sck_o(spi_sck_o),
+        .spi_cs_no(spi_cs_no),
+        .spi_rx_i(spi_poci_i),
+        .spi_tx_o(spi_pico_o)
     );
 
     task reset;
+        $display("[%t]    spi1.reset()", $time);
         spi.reset();
     endtask
 
@@ -70,31 +45,6 @@ module spi1_driver #(
         return addr[7:0];
     endfunction
 
-    // We allow 
-    logic        expected_rd_index = '0;
-    logic        expected_wr_index = '0;
-    logic [16:0] expected_addr_fifo [2];
-    logic        expected_we_fifo   [2];
-    logic [7:0]  expected_data_fifo [2];
-
-    wire [16:0] expected_addr = expected_addr_fifo[expected_rd_index];
-    wire        expected_we   = expected_we_fifo[expected_rd_index];
-    wire [7:0]  expected_data = expected_data_fifo[expected_rd_index];
-
-    wire [16:0] last_addr     = expected_addr_fifo[expected_wr_index - 1'b1];
-
-    task set_expected(
-        input [16:0] addr_i,
-        input        we_i,
-        input [7:0]  data_i = 8'hxx
-    );
-        expected_addr_fifo[expected_wr_index]  <= addr_i;
-        expected_data_fifo[expected_wr_index]  <= data_i;
-        expected_we_fifo[expected_wr_index]    <= we_i;
-
-        expected_wr_index <= expected_wr_index + 1'b1;
-    endtask
-
     task write_at(
         input [16:0] addr_i,
         input [7:0] data_i
@@ -104,8 +54,6 @@ module spi1_driver #(
         logic [7:0] al;
 
         $display("[%t]    spi1.write_at(%x, %x)", $time, addr_i, data_i);
-
-        set_expected(/* addr: */ addr_i, /* we: */ 1'b1, /* data: */ data_i);
 
         c = cmd(/* rw_n: */ '0, /* set_addr: */ 1'b1, addr_i);
         ah = addr_hi(addr_i);
@@ -121,7 +69,7 @@ module spi1_driver #(
         logic [7:0] ah;
         logic [7:0] al;
 
-        set_expected(/* addr: */ addr_i, /* we: */ '0);
+        $display("[%t]    spi1.read_at(%x, %x)", $time, addr_i);
 
         c = cmd(/* rw_n: */ 1'b1, /* set_addr: */ 1'b1, addr_i);
         ah = addr_hi(addr_i);
@@ -133,7 +81,7 @@ module spi1_driver #(
     task read_next();
         logic [7:0] c;
 
-        set_expected(/* addr: */ last_addr + 1'b1, /* we: */ '0);
+        $display("[%t]    spi1.read_next()", $time);
 
         c = cmd(/* rw_n: */ 1'b1, /* set_addr: */ 1'b0, 6'bxxxxxx);
 
@@ -146,43 +94,4 @@ module spi1_driver #(
     );
         write_at(17'he80f, { 6'h00, ready, !reset });
     endtask
-
-    always @(negedge cs_n) begin
-        @(posedge clk_i)    // 2FF stage 1
-        @(posedge clk_i)    // 2FF stage 2
-        @(posedge clk_i)    // Edge detect
-        #1 assert(!spi_ready) else begin
-            $error("Asserting 'spi1_master_cs_n' must reset 'spi_ready'.  (spi_cs_n=%d, spi_ready=%d)", cs_n, spi_ready);
-            $finish;
-        end
-    end
-
-    always @(negedge cs_n) begin
-        @(posedge clk_i)    // 2FF stage 1
-        @(posedge clk_i)    // 2FF stage 2
-        @(posedge clk_i)    // Edge detect
-        #1 assert(!cycle_o) else begin
-            $error("Deasserting 'spi1_master_cs_n' must reset 'cycle_o'.  (spi_cs_n=%d, cycle_o=%d)", cs_n, cycle_o);
-            $finish;
-        end
-    end
-
-    always @(posedge cycle_o) begin
-        #1 assert(addr_o == expected_addr) else begin
-            $error("'addr_o' must produce expected address.  (expected=%h, actual=%h)", expected_addr, addr_o);
-            $finish;
-        end
-
-        assert(we_o == expected_we) else begin
-            $error("'we_o' must produce expected rw_n.  (expected=%h, actual=%h)", expected_we, we_o);
-            $finish;
-        end
-
-        assert(!we_o || wr_data_o == expected_data) else begin
-            $error("'wr_data_o' must produce expected data when writing.  (expected=%h, actual=%h)", expected_data, wr_data_o);
-            $finish;
-        end
-
-        expected_rd_index <= expected_rd_index + 1'b1;
-    end
 endmodule
--- gw/PET/sim/spi1_tb.sv
+++ gw/PET/sim/spi1_tb.sv
@@ -15,33 +15,138 @@
  `timescale 1ns / 1ps
 
 module spi1_tb;
-    bit clk_sys = '0;
-    initial forever #(1000 / (64 * 2)) clk_sys = ~clk_sys;
+    bit clk = '0;
+    initial forever #(1000 / (64 * 2)) clk = ~clk;
 
-    logic [16:0] spi_addr;
-    logic  [7:0] spi_wr_data;
-    logic  [7:0] spi_rd_data;
-    logic        spi_we;
-    logic        spi_cycle;
+    logic spi_sck;
+    logic spi_cs_n;
+    logic spi_pico;
+    logic spi_poci;
 
-    spi1_driver driver(
-        .clk_i(clk_sys),
-        .addr_o(spi_addr),
-        .we_o(spi_we),
-        .cycle_o(spi_cycle)
+    spi1_driver spi1_driver(
+        .spi_sck_o(spi_sck),
+        .spi_cs_no(spi_cs_n),
+        .spi_pico_o(spi_pico),
+        .spi_poci_i(spi_poci)
     );
+
+    logic [16:0] addr;
+    logic  [7:0] rd_data;
+    logic  [7:0] wr_data;
+    logic        we;
+    logic        cycle;
+    logic        spi_ready;
+
+    spi1_master spi1(
+        .spi_sck_i(spi_sck),
+        .spi_cs_ni(spi_cs_n),
+        .spi_rx_i(spi_pico),
+        .spi_tx_o(spi_poci),
+        .spi_ready_o(spi_ready),
+
+        .clk_i(clk),
+        .addr_o(addr),
+        .rd_data_i(rd_data),
+        .wr_data_o(wr_data),
+        .we_o(we),
+        .cycle_o(cycle)
+    );
+
+    logic        expected_rd_index = '0;
+    logic        expected_wr_index = '0;
+    logic [16:0] expected_addr_fifo [2];
+    logic        expected_we_fifo   [2];
+    logic [7:0]  expected_data_fifo [2];
+
+    wire [16:0] expected_addr = expected_addr_fifo[expected_rd_index];
+    wire        expected_we   = expected_we_fifo[expected_rd_index];
+    wire [7:0]  expected_data = expected_data_fifo[expected_rd_index];
+
+    wire [16:0] last_addr     = expected_addr_fifo[expected_wr_index - 1'b1];
+
+    task set_expected(
+        input [16:0] addr_i,
+        input        we_i,
+        input [7:0]  data_i = 8'hxx
+    );
+        expected_addr_fifo[expected_wr_index]  <= addr_i;
+        expected_data_fifo[expected_wr_index]  <= data_i;
+        expected_we_fifo[expected_wr_index]    <= we_i;
+
+        expected_wr_index <= expected_wr_index + 1'b1;
+    endtask
+
+    task write_at(
+        input [16:0] addr_i,
+        input [7:0] data_i
+    );
+        set_expected(/* addr: */ addr_i, /* we: */ 1'b1, /* data: */ data_i);
+        spi1_driver.write_at(addr_i, data_i);
+    endtask
+
+    task read_at(
+        input [16:0] addr_i
+    );
+        set_expected(/* addr: */ addr_i, /* we: */ '0);
+        spi1_driver.read_at(addr_i);
+    endtask
+
+    task read_next();
+        set_expected(/* addr: */ last_addr + 1'b1, /* we: */ '0);
+        spi1_driver.read_next();
+    endtask
+
+    always @(negedge spi_cs_n) begin
+        @(posedge clk)  // 2FF stage 1
+        @(posedge clk)  // 2FF stage 2
+        @(posedge clk)  // Edge detect
+        
+        #1;
+        
+        assert(!spi_ready) else begin
+            $error("Asserting 'spi1_cs_n' must reset 'spi_ready'.  (spi_cs_n=%d, spi_ready=%d)", spi_cs_n, spi_ready);
+            $finish;
+        end
+
+        assert(!cycle) else begin
+            $error("Asserting 'spi1_cs_n' must reset 'cycle'.  (spi_cs_n=%d, cycle=%d)", spi_cs_n, cycle);
+            $finish;
+        end
+    end
+
+    always @(posedge cycle) begin
+        #1 $display("[%t]        (cycle = %b, addr = %x, we = %b, wr_data = %x)", $time, cycle, addr, we, wr_data);
+
+        assert(addr == expected_addr) else begin
+            $error("'addr' must produce expected address.  (expected=%h, actual=%h)", expected_addr, addr);
+            $finish;
+        end
+
+        assert(we == expected_we) else begin
+            $error("'we' must produce expected rw_n.  (expected=%h, actual=%h)", expected_we, we);
+            $finish;
+        end
+
+        assert(!we || wr_data == expected_data) else begin
+            $error("'wr_data' must produce expected data when writing.  (expected=%h, actual=%h)", expected_data, wr_data);
+            $finish;
+        end
+
+        expected_rd_index <= expected_rd_index + 1'b1;
+    end
 
     initial begin
         $dumpfile("work_sim/out.vcd");
         $dumpvars(0, spi1_tb);
 
-        driver.reset();
-        driver.write_at(17'h00000, 8'h00);
-        driver.read_next(8'h01);
-        driver.read_next(8'h01);
-        driver.read_next(8'h01);
-        driver.read_next(8'h01);
-        driver.read_next(8'h01);
+        spi1_driver.reset();
+        
+        write_at(17'h00000, 8'h00);
+        read_next(8'h01);
+        read_next(8'h01);
+        read_next(8'h01);
+        read_next(8'h01);
+        read_next(8'h01);
 
         #100
 
