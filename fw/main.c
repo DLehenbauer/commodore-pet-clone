dc497ed7d4309fdeaf9a344f1290527626f5542c
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2023-11-23 14:46:59 +0000

    fw(rev-b): light LED during configuration

--- fw/main.c
+++ fw/main.c
@@ -30,6 +30,13 @@
 #include "usb/usb.h"
 
 void init() {
+    // Turn on LED at start of config to signal that the RP2040 has booted and FPGA
+    // configuration has started.  (Note that 'usb_init()' will turn the LED off.)
+    const uint led_pin = PICO_DEFAULT_LED_PIN;
+    gpio_init(led_pin);
+    gpio_set_dir(led_pin, GPIO_OUT);
+    gpio_put(led_pin, 1);
+
     fpga_init();
     fpga_config();
     printf("FPGA initialized.\n");
